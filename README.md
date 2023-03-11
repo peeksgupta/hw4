21e4fc3bb3af17408a07522678045e0b337afde4
peeksgupta <pulkit.gupta@kellogg.northwestern.edu>
2023-03-11 18:30:34 +0000

    Final Commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # ENTR-451 Homework #4
 
 Full instructions at https://entr451.com/homework-4/
+My app --> https://travel-journal-app.herokuapp.com/
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,5 @@
+
+
 class SessionsController < ApplicationController
   def new
   end
@@ -10,18 +12,18 @@ class SessionsController < ApplicationController
         session["user_id"] = @user["id"]
         redirect_to "/places"
       else
-        flash["notice"] = "Incorrect details Entered!"
+        flash["notice"] = "Incorrect credentials!"
         redirect_to "/login"
       end
     else
+      flash["notice"] = "Incorrect credentials!"
       redirect_to "/login"
     end
   end
 
   def destroy
     session["user_id"] = nil
-    flash["notice"] = "Visit Again!"
+    flash["notice"] = "Visit again!"
     redirect_to "/login"
   end
 end
-  
